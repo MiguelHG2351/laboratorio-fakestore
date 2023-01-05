db1ad18ac6c6c091fdefc807e1feaf5db1e9c73a
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-01-05 19:04:51 +0000

    feat: implement render of cards

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://api.escuelajs.co/api/v1/products';
+const API_URL = new URL('https://api.escuelajs.co/api/v1/products?limit=10');
 
 const getData = api => {
-  fetch(api)
+  console.log(api.searchParams.set('offset', localStorage.getItem('pagination')))
+  fetch(api.toString())
     .then(response => response.json())
     .then(response => {
+      console.log(response[0].images)
       let products = response;
       let output = products.map(product => {
-        // template
+        const temp = `<article class="Card">
+        <img src="${product.images[0]}" />
+        <h2>
+          ${product.title}
+          <small>$ ${product.price}</small>
+        </h2>
+      </article>`
+      return temp;
       });
       let newItem = document.createElement('section');
-      newItem.classList.add('Item');
-      newItem.innerHTML = output;
+      newItem.classList.add('Items');
+      newItem.innerHTML = output.join('');
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
 }
 
 const loadData = () => {
-  getData(API);
+  getData(API_URL);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  localStorage.setItem('pagination', 5);
+  loadData()
+})
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
