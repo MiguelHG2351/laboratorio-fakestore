15ebaedd4bf15517c1ceb6cbc3e5dc9c7fe84388
Miguel Hernández Gaitan <miguelhernandezgaitan13@gmail.com>
2023-01-05 19:57:48 +0000

    feat:unobserver whhen element have all cards

--- src/index.js
+++ src/index.js
@@ -2,33 +2,38 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API_URL = new URL('https://api.escuelajs.co/api/v1/products?limit=10');
 
-const getData = api => {
-  console.log(api.searchParams.set('offset', localStorage.getItem('pagination')))
-  fetch(api.toString())
-    .then(response => response.json())
-    .then(response => {
-      console.log(response[0].images)
-      let products = response;
-      let output = products.map(product => {
-        const temp = `<article class="Card">
-        <img src="${product.images[0]}" />
+const getData = async api => {
+  let offset = localStorage.getItem('pagination') - 0;
+  console.log(api.searchParams.set('offset', offset))
+  try {
+    const response = await fetch(api.toString());
+    const response_2 = await response.json();
+    console.log(response_2);
+    let products = response_2;
+    if(products.length === 0) {
+      intersectionObserver.unobserve($observe);
+      return alert('No hay mas productos');
+    }
+    let output = products.map(product => {
+      const temp = `<article class="Card">
+        <img src="${product.images[0]}" width={640} height={480} />
         <h2>
           ${product.title}
           <small>$ ${product.price}</small>
         </h2>
-      </article>`
+      </article>`;
       return temp;
-      });
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output.join('');
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
+    });
+    let container = document.querySelector('section');
+    container.innerHTML += output.join('');
+    localStorage.setItem('pagination', offset+=10);
+  } catch (error) {
+    return console.log(error);
+  }
 }
 
-const loadData = () => {
-  getData(API_URL);
+const loadData = async () => {
+  await getData(API_URL);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -37,9 +42,13 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  // logic...
+  const docLength = document.querySelector('.Items').children.length
+  if (entries[0].isIntersecting && docLength >= 10) {
+    console.log('Cargar mas productos');
+    loadData();
+  }
 }, {
-  rootMargin: '0px 0px 100% 0px',
+  // rootMargin: '0px 0px 100% 0px',
+  threshold: 1
 });
-
 intersectionObserver.observe($observe);
